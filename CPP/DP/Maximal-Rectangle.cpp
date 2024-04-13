7da06d4305d2d63cf7e7415df834b73a5072711e
classicalbit <deepanshu.mishra0@gmail.com>
2024-04-13 22:57:29 +0530

    re-structure

---
+++ CPP/DP/Maximal-Rectangle.cpp
@@ -0,0 +1,31 @@
+/*
+85. Maximal Rectangle
+Input: matrix = [["1","0","1","0","0"],
+                 ["1","0","1","1","1"],
+                 ["1","1","1","1","1"],
+                 ["1","0","0","1","0"]]
+Output: 6
+Explanation: The maximal rectangle is shown in the above picture.
+Example 2:
+
+Input: matrix = [["0"]]
+Output: 0
+Example 3:
+
+Input: matrix = [["1"]]
+Output: 1
+*/
+
+#include <iostream>
+using namespace std;
+
+int maximal_rectangle(vector<vector<char>> &matrix)
+{
+}
+
+int main()
+{
+    vector<vector<char>> m;
+    for (int i)
+        cout << maximal_rectangle(m);
+}
