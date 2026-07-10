8f1d12ca7c25464073f65ef4a7129a4a441b5442
classical-bit <deepanshu.mishra0@gmail.com>
2026-07-10 19:58:31 +0530

    Factorial modulo

---
+++ comp-prog/factorial-modulo.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int x = 1;
+    int n = 5;
+    int m = 11;
+    for (int i = 2; i <= n; i++) {
+        x = (x* i) % m;
+    }
+    cout << x % m << "\n";
+    return 0;
+}
