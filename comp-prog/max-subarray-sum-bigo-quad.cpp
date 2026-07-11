54ae0f43febe3832c1bcf3790d71d7d13cbeac96
classical-bit <deepanshu.mishra0@gmail.com>
2026-07-11 20:32:59 +0530

    Maximum subarray sum - Quadratic solution

---
+++ comp-prog/max-subarray-sum-bigo-quad.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int array[] = {-1,2,4,-3,5,2,-5,2};
+    int n = size(array);
+
+    int best = 0;
+    for (int a = 0; a < n; a++) {
+        int sum = 0;
+        for (int b = a; b < n; b++) {
+            sum += array[b];
+            best = max(best, sum);
+        }
+    }
+    cout << best << "\n";
+    return 0;
+}
