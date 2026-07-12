697cf913898b9a7d42d0c9f9bb61b5d513785610
classical-bit <deepanshu.mishra0@gmail.com>
2026-07-12 18:33:41 +0530

    Merge sort  n-Logarithmic algorithm

---
+++ comp-prog/merge-sort-bigo-nlogarithmic.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void merge(int array[],int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    vector<int> L(n1);
+    vector<int> R(n2);
+
+    for (int i = 0; i < n1 ; i++)
+        L[i] = array[left + i];
+    for (int i = 0; i < n2; i++)
+        R[i] = array[mid + 1 + i];
+
+    int i = 0;
+    int j = 0;
+    int k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            array[k] = L[i];
+            i++;
+        } else {
+            array[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        array[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        array[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int array[], int left, int right) {
+    if (left >= right) return;
+
+    int mid = left + (right - left) /2;
+    mergeSort(array, left, mid);
+    mergeSort(array, mid + 1, right);
+
+    merge(array, left, mid, right);
+}
+
+int main() {
+    int array[] = {1,3,8,2,9,2,5,6};
+    int n = size(array);
+
+    mergeSort(array, 0, n - 1);
+
+    for (auto x: array)
+        cout << x << ", ";
+    cout << "\n";
+    return 0;
+}
